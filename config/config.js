3a2d11a8994bbedb176a94d7c950076f1e6f31e6
georgeCentennial <gcayaby2@my.centennialcollege.ca>
2025-03-21 23:16:56 -0400

    added changes to config file

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
   env: process.env.NODE_ENV || 'development', 
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
-  mongoUri: process.env.MONGODB_URI ||"mongodb+srv://gcayaby2:uLMo5n4oSu24FD5o@cluster0.vk2s9.mongodb.net/Skeleton?retryWrites=true&w=majority&appName=Cluster0" ||
+  mongoUri: process.env.MONGODB_URI ||"mongodb+srv://gcayaby2:uLMo5n4oSu24FD5o@cluster0.vk2s9.mongodb.net/RCentS?retryWrites=true&w=majority&appName=Cluster0" ||
   process.env.MONGO_HOST ||
   'mongodb://' + (process.env.IP || 'localhost') + ':' + 
  (process.env.MONGO_PORT || '27017') +
