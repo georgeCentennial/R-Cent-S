e88d62308ce720bf9bb4ace91313d4a47307841f
VieGuese <maivyguese@gmail.com>
2025-04-04 19:25:38 -0400

    Files for SIGN IN and SIGN UP

--- template.js
+++ template.js
@@ -4,13 +4,11 @@ export default () => {
     <head>
         <meta charset="utf-8">
         <title>MERN Skeleton</title>
-        <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:100,300,400"> 
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" 
-        rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     </head>
     <body>
-        <div id="root">THIS IS THE PROJECT 1</div>
+        <div id="root">Hello World</div>
         <script type="text/javascript" src="/dist/bundle.js"></script>
     </body>
     </html>`;
