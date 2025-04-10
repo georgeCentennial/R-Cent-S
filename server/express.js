4b853a2e1e7e9dfd3fa47b3638e0616e1b978a3c
georgeCentennial <gcayaby2@my.centennialcollege.ca>
2025-04-09 22:09:24 -0400

    added cors origin

--- server/express.js
+++ server/express.js
@@ -29,7 +29,10 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+    origin: 'https://r-cent-s-g8u3.vercel.app/',
+    credentials: true,
+}))
 app.use((err, req, res, next) => {
     if (err.name === 'UnauthorizedError') {
         res.status(401).json({"error" : err.name + ": " + err.message}) 
